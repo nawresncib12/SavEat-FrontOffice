7212129fae0529ac97aaf8c7661a3efb28f48417
nawresncib12 <nawresncib@insat.u-carthage.tn>
2022-03-18 21:00:27 +0100

    background

--- saveat/src/components/auth/Background.jsx
+++ saveat/src/components/auth/Background.jsx
@@ -1,10 +1,18 @@
 import classes from "./Background.module.css";
+import FormSection from "./FormSection";
+import WelcomeSection from "./WelcomeSection";
 const Background = () => {
-  return <div className={classes.authBackground}>
+  return (
+    <div className={classes.authBackground}>
       <div className={classes.authHead}>
-          <img src="assets/logo.png" alt="" />
+        <img src="assets/logo.png" alt="" />
       </div>
-  </div>;
+      <div className={classes.authBody}>
+        <WelcomeSection></WelcomeSection>
+        <FormSection></FormSection>
+      </div>
+    </div>
+  );
 };
 
 export default Background;
---
+++ saveat/src/components/auth/FormSection.jsx
@@ -0,0 +1,9 @@
+import classes from "./FormSection.module.css";
+const FormSection = () => {
+  return (
+    <div className={classes.formSection}>
+    </div>
+  );
+};
+
+export default FormSection;
