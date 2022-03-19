688f70281fb18f96c70a91de3a0546cbeb835c0b
nawresncib12 <nawresncib@insat.u-carthage.tn>
2022-03-19 17:12:48 +0100

    background is responsive

--- saveat/src/components/auth/FormSection.jsx
+++ saveat/src/components/auth/FormSection.jsx
@@ -1,7 +1,10 @@
 import classes from "./FormSection.module.css";
-const FormSection = () => {
+const FormSection = (props) => {
   return (
     <div className={classes.formSection}>
+      <div className={classes.whiteWave}>
+        <img src="assets/whitewave.png" alt="whitewave" />
+      </div>
     </div>
   );
 };
