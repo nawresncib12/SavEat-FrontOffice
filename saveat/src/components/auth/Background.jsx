239657b76e9cb7029c56b4e99a3bc6e586f995bc
meriem <meriembenismail@insat.u-carthage.tn>
2022-03-18 21:31:44 -0700

    sign in form ui added

--- saveat/src/components/auth/Background.jsx
+++ saveat/src/components/auth/Background.jsx
@@ -1,9 +1,11 @@
 import classes from "./Background.module.css";
+import { SignInForm } from "./SignInForm";
 const Background = () => {
   return <div className={classes.authBackground}>
       <div className={classes.authHead}>
           <img src="assets/logo.png" alt="" />
       </div>
+      <SignInForm />
   </div>;
 };
 
