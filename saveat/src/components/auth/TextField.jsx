59eef85aa237e2e276ab45b2b01937303f2aaf58
meriem <meriembenismail@insat.u-carthage.tn>
2022-03-21 17:51:59 -0700

    border color change on focus input boxes

--- saveat/src/components/auth/TextField.jsx
+++ saveat/src/components/auth/TextField.jsx
@@ -7,43 +7,61 @@ export const TextField = ({ label, ...props }) => {
   const [field, meta] = useField(props);
   let styleError;
   let styleErrorInput;
-  let styleErrorIcon;
   const [passVisible,setPassVisible] = useState(false);
   const [labelColor,setLabelColor] = useState("#A5A5A5");
-
+  const [border,setBorder] = useState("1.5px solid #A5A5A5");
+  const [borderDiv,setBorderDiv] = useState("2px solid #A5A5A5");
+  const [isFocus,setIsFocus] = useState(false);
+  const changeColor = () => {
+    setIsFocus(true);
+    setLabelColor("#4DAAAA");
+    setBorder("1.5px solid #4DAAAA");
+    setBorderDiv("2px solid #4DAAAA");
+  }
+  const resetColor = () => {
+    setIsFocus(false);
+    console.log('hello')
+  }
+  
+  
+  
   if(meta.touched && meta.error) {
     styleError ={
-      border: "1px solid red"
+      border: "2px solid red"
     }
     styleErrorInput ={
-      borderTop: "1px solid red",
-      borderBottom: "1px solid red"
+      borderTop: "1.5px solid red",
+      borderBottom: "1.5px solid red"
     }
-    styleErrorIcon = {
-      display:"block"
-    }
-
   }
   const passwordVisible = () => {
     setPassVisible(!passVisible);
 
   }
-  const changeColor = () => {
-    setLabelColor("#4DAAAA")
-    styleError = {
-      border: "3px solid #4DAAAA"
 
-    }
-  }
+
+
+ let style ;
+ const getStyle = () =>  {
+   if(meta.touched && meta.error) {
+     style=styleError
+   } else 
+   if(isFocus) {
+     style={border:borderDiv}
+   } 
+  
+ }
+ 
   return (
     <div className={classes.inputBox}>
       <label htmlFor={field.name} style={{color:labelColor}}>{label}</label>
-      <div className={classes.input} style={styleError} >
-       
-        <input onFocus={changeColor} 
+      {getStyle()}
+      <div className={classes.input} style={style}  >
+        
+        <input onFocus={changeColor} onBlur={resetColor}
           className={classes.formControl}
           {...field} {...props} 
-          autoComplete="off" style={styleErrorInput} type={passVisible && props.type === "password" ? "text" : props.type }
+          autoComplete="off" style={isFocus && !meta.touched && !meta.error ?  {borderTop:border,borderBottom:border} : styleErrorInput } type={passVisible && props.type === "password" ? "text" : props.type }
         />
         { props.type === "password" && <span onClick={passwordVisible} className={classes.passIcon}><Icon icon="akar-icons:eye" /></span>}
         
