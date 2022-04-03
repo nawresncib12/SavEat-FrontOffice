3fd2383a78b62924cf995f61cc4cbfa82ef1f394
nawresncib12 <nawresncib@insat.u-carthage.tn>
2022-04-03 17:10:59 +0100

    linked logout

--- saveat/src/pages/login.jsx
+++ saveat/src/pages/login.jsx
@@ -1,9 +1,15 @@
 import Background from "../components/auth/Background/Background";
-
+import { loggedIn } from "../api/api.user";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 const Login = () => {
-  return <Background auth={false}>
-
-  </Background>;
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (loggedIn()) {
+      navigate("/home");
+    }
+  }, [navigate]);
+  return <Background auth={false}></Background>;
 };
 
 export default Login;
