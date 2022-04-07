da71b58ba276361e3262870b766fdeae3a70505d
nawresncib12 <nawresncib@insat.u-carthage.tn>
2022-04-07 15:57:06 +0100

    auth check

--- saveat/src/pages/login.jsx
+++ saveat/src/pages/login.jsx
@@ -1,24 +1,41 @@
 import Background from "../components/auth/Background/Background";
 import { loggedIn } from "../api/api.user";
 import { useNavigate, useLocation } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect , useState} from "react";
 
+import { LoaderPage } from "./loader";
 const Login = () => {
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
   useEffect(() => {
-    if (loggedIn()) {
-      navigate("/home");
+    navigate("/login");
+    async function log() {
+      if (await loggedIn()) {
+        setLoading(false);
+        navigate("/home");
+      } else {
+        setLoading(false);
+      }
     }
+    log();
   }, [navigate]);
-  const {state} = useLocation();
-  
-  let toggle ;
-  if(state){
-    toggle =state.toggle 
-  }else{
+  const { state } = useLocation();
+
+  let toggle;
+  if (state) {
+    toggle = state.toggle;
+  } else {
     toggle = false;
   }
-  return <Background toggle={toggle}  auth={false}></Background>;
+  return (
+    <>
+      {loading ? (
+        <LoaderPage />
+      ) : (
+        <Background toggle={toggle} auth={false}></Background>
+      )}
+    </>
+  );
 };
 
 export default Login;
