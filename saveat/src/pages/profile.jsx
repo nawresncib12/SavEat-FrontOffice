d0fc4f5a55690f5249c9cdb68670d1bcdab86661
SamerBenMim <samerbenmim@insat.u-carthage.tn>
2022-04-13 17:41:23 +0100

    progress bar

--- saveat/src/pages/profile.jsx
+++ saveat/src/pages/profile.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import SideBar from '../components/SideBar'
+import ProfileCard from '../components/ProfileCard'
 const Profile = () => {
   return (
     <div style={{display:"flex",height:"100%"}}>
         <SideBar/>
-        {/* <div style={{width:'75%',backgroundColor:"blue"}}></div> */}
+        <div style={{width : "100%"}}></div>
+        <ProfileCard/>
     </div>
   )
 }
