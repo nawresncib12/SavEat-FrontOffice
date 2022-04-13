a28e133ff29d72a4da0a67956cec5e78b867dfb8
SamerBenMim <samerbenmim@insat.u-carthage.tn>
2022-04-13 03:09:49 +0100

    SideBar

---
+++ saveat/src/pages/profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SideBar from '../components/SideBar'
+const Profile = () => {
+  return (
+    <div style={{display:"flex",height:"100%"}}>
+        <SideBar/>
+        {/* <div style={{width:'75%',backgroundColor:"blue"}}></div> */}
+    </div>
+  )
+}
+
+export default Profile
