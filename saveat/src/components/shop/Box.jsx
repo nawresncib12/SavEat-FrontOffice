31e68a74286ab538d29c8d77887b0ae8dcaa1e13
Samer <samerbenmim@insat.u-carthage.tn>
2022-05-30 01:30:09 +0100

    fix

--- saveat/src/components/shop/Box.jsx
+++ saveat/src/components/shop/Box.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Button } from '../../UI/Button';
 import classes from './Box.module.css';
 import avatar1 from '../../assets/avatar1.svg';
-import { useState } from 'react';
 import { connect } from 'react-redux';
 import { addToCart } from '../redux/actions/actions';
+import { increment_quantity } from '../redux/actions/actions';
+import style from "../profile/CartLine.module.css";
+import { Icon } from '@iconify/react';
 
-const Box = ({id,category,subcategory,price,stock,items,addToCart}) => {
+const Box = ({id,category,subcategory,price,stock,items,addToCart,increment_quantity}) => {
 
+    const [quantity, setQuantity] = useState(0)
+    const handlePlus=()=>{
+        setQuantity(quantity+1)
+        increment_quantity(id,1)
+    }
+    const handleMinus=()=>{
+        if(quantity>0){
 
+            setQuantity(quantity-1)
+            increment_quantity(id,-1)
+
+        }
+    }
   return (
     <div className={classes.box}>
         <h2 className={classes.title}>
@@ -27,11 +41,41 @@ const Box = ({id,category,subcategory,price,stock,items,addToCart}) => {
         {
             stock > 0 ? 
             <div>
-              
-               <Button shake={true} color="#4DAAAA" content={"Add To Cart" }  onClick={()=>{ 
-                   let quantity=1; 
-               if(stock) addToCart({category,subcategory,price,items,id,quantity})
-            }}/> 
+
+             {(quantity==0)?
+
+                 <Button shake={true} color="#4DAAAA" content={"Add To Cart" }  onClick={()=>{ 
+                     setTimeout(() => {
+                         setQuantity(quantity+1)
+                         increment_quantity(id,1)
+
+                     }, 600);
+                     addToCart({category,subcategory,price,items,id,quantity})
+                    }}/> 
+                    :
+
+
+
+
+
+
+    <div className={style.options} style={{display:"flex" , fontFamily:"19px"}}>
+    <Icon icon="akar-icons:circle-minus" color="#4DAAAA" width="25px" height="25px" style={{cursor:"pointer"}}
+     onClick={handleMinus} 
+     />
+    <span className={style.unselectable}  style={{margin : "0 15px 0px 15px ",width:"minContent"}}>{quantity}</span>
+    <Icon icon="akar-icons:circle-plus" color="#4DAAAA" width="25px" height="25px" style={{cursor:"pointer"}} 
+    onClick={handlePlus}
+     />
+
+    
+  </div>
+
+
+
+
+          
+            } 
                
                
             </div> : 
@@ -44,7 +88,9 @@ const Box = ({id,category,subcategory,price,stock,items,addToCart}) => {
 
 const mapDispatchToProps=(dispatch)=>{
     return {
-        addToCart : (productInfo)=> dispatch(addToCart(productInfo))
+        increment_quantity : (id,step)=> dispatch(increment_quantity(id,step)),
+        addToCart : (productInfo)=> dispatch(addToCart(productInfo)),
+
     }
   }
   const mapStateToProps=(state)=>{
